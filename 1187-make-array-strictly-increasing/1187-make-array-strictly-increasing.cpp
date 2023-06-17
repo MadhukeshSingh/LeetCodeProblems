b3d370bdd41e7668a59a24c1c0ce67cd337203d4
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-17 16:43:39 +0530

    Time: 688 ms (28.49%), Space: 78 MB (16.86%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-     int solve(int i,int j,int &a,int &b,int p,vector<vector<int>> &dp,vector<int> &v1,vector<int> &v2){
-        if(i==a){
-            return 0;
+     int c(int i,int prev,vector<int>&arr1,vector<int>&arr2,vector<unordered_map<int,int>>&dp){
+        if(i>=arr1.size())return 0;
+        if(dp[i].find(prev)!=dp[i].end())return dp[i][prev];
+        int r1=INT_MAX,r2=INT_MAX;
+        if(arr1[i]>prev){
+            r1=c(i+1,arr1[i],arr1,arr2,dp);
         }
-        j = upper_bound(v2.begin()+j,v2.end(),p)-v2.begin();
-        if(dp[i][j] != -1)return dp[i][j];
-        if(j==b && v1[i]<=p)return 2001;
-        int take = 2001,nottake = 2001;
-        if(j!=b)
-        take = solve(i+1,j+1,a,b,v2[j],dp,v1,v2)+1;
-        if(v1[i]>p)
-        nottake = solve(i+1,j,a,b,v1[i],dp,v1,v2);
-        return dp[i][j] = min(take,nottake);
+        int in=upper_bound(arr2.begin(),arr2.end(),prev)-arr2.begin();
+        if(in!=arr2.size()){
+            r2=c(i+1,arr2[in],arr1,arr2,dp);
+        }
+        if(r2==INT_MAX){
+            return dp[i][prev]=r1;
+        }
+        else return dp[i][prev]=min(r1,r2+1);
     }
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size(),m=arr2.size();
-        vector<vector<int>> dp(2001,vector<int>(2001,-1));
         sort(arr2.begin(),arr2.end());
-        int a= solve(0,0,n,m,-1,dp,arr1,arr2);
-        if(a>n)return -1;
-        return a;
+         vector<unordered_map<int,int>>dp(2020);
+            int val=c(0,INT_MIN,arr1,arr2,dp); 
+            if(val==INT_MAX)return -1;
+            else return val; 
     }
 };
