87c40f58151527e4afa252ddb712fa05ae398c13
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-19 17:49:37 +0530

    Time: 4 ms (78.56%), Space: 10.4 MB (11.22%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,27 +11,49 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-         ListNode* temp = head  ; 
-    
-    ListNode* left = new ListNode(0);
-    ListNode* right = new ListNode(0) ; 
-    ListNode* dummy1 = left  ;
-    ListNode* dummy2 = right ; 
-    while(temp!=NULL){
-        if(temp->val < x){
-            dummy1->next = temp ; 
-            dummy1 = dummy1->next ; 
-         }
-        else {
-            dummy2->next = temp ; 
-            dummy2 = dummy2->next ; 
-            
+      if(head == NULL || head ->next == NULL){
+            return head;
         }
-        temp = temp->next ; 
-    }
-    dummy1->next = right->next ; 
-    dummy2->next = NULL;
-    
-    return left->next ; 
+        queue<ListNode*>q;
+        ListNode* lar = head;
+        while(lar != NULL){
+            if(lar ->val >= x){
+                break;
+            }
+            lar = lar ->next;
+        }
+        ListNode* temp = head;
+        ListNode* prev = head;
+        ListNode* back = NULL;
+        while(temp != NULL){
+            temp = temp ->next;
+            if(prev ->val >=x){
+                back = prev;
+            }
+            if(prev ->val <x){
+                prev ->next = NULL;
+                if(back != NULL){
+                    back ->next = temp;
+                }
+                q.push(prev);
+            }
+            prev = temp;
+        }
+        ListNode* temp1 = NULL;
+        if(q.empty()==false){
+            ListNode* l = q.front();
+            temp1 = l;
+            q.pop();
+            while(!q.empty()){
+                l ->next = q.front();
+                q.pop();
+                l = l ->next;
+            }
+            if(l != NULL){
+                l ->next = lar;
+            }
+            return temp1;
+        }
+        return head;
     }
 };
