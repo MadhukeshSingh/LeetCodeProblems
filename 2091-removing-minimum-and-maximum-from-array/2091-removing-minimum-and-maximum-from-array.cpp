5de2fe4797d5c7429a400e942c99939b73d35cb7
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-21 22:44:17 +0530

    Time: 176 ms (82.12%), Space: 88.2 MB (67.24%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        // remove untill it max of min
-        
-        int n = nums.size();
-        if( n == 1){
-            return 1;
-        }
-        if ( n==2){
-            return 2;
-        }
-        // find min 
-        int minindex=0;
-        int min_el=INT_MAX;
-        for ( int i=0; i< n;i++){
-            
-            min_el= min(min_el, nums[i]);
-           
-        }
-        for ( int i=0;i< n;i++){
-            if (nums[i]==min_el){
-                minindex =i;
+       int n=nums.size();int minnum=nums[0], maxnum=nums[0] ,minindex=0, maxindex=0;
+        int a=0,b=0;
+        if(n==1)return 1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<minnum){
+                minnum=nums[i];
+                minindex=i;
+            }
+            if(nums[i]>maxnum){
+                maxnum=nums[i];
+                maxindex=i;
             }
         }
-        // find max
-        int mxindex=0;
-        int max_el=INT_MIN;
-        for ( int i=0; i< n;i++){
-            
-            max_el=  max( max_el, nums[i]);
-           
-        }
-        for ( int i=0;i< n;i++){
-            if (nums[i]== max_el){
-                mxindex =i;
-            }
-        }
-        int high = max(mxindex,minindex);
-        int low = min(mxindex,minindex);
-        
-        int t = min(high+1,n-low);
-        return min(t, low + n-high + 1);
+   
+a=min( n-min(minindex,maxindex), max(minindex,maxindex)+1);
+
+b= n - max(minindex,maxindex) + min(minindex,maxindex) + 1;
+
+      return min(a,b);
     }
 };
