390a011b5305987887a24e0e279c5f9da30a6760
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-23 14:47:27 +0530

    Time: 193 ms (86.33%), Space: 109.7 MB (37.89%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int sum = 0, i, size = nums.size();
-    double maxKAverage=INT_MIN, avg;
-    for (i = 0; i < size; ++i) {
-        if (i < k) {
-            sum+=nums[i];
-        } else {
-            sum+=nums[i]-nums[i-k];
-        }
-        if (i >= k-1) {
-            avg = static_cast<double>(sum)/k;
-            if (maxKAverage < avg) {
-                maxKAverage = avg;
+        int i = 0, j = 0;
+        double sum = 0;
+        int n = nums.size();
+        double maxAvg = INT_MIN;
+        
+        while (j < n) {
+            sum += nums[j];
+            
+            if (j - i + 1 < k) {
+                j++;
+            }
+            else if (j - i + 1 == k) {
+                maxAvg = std::max(maxAvg, sum / k);
+                sum -= nums[i];
+                i++;
+                j++;
             }
         }
-    }
-    return maxKAverage;
+        
+        return maxAvg;
     }
 };
