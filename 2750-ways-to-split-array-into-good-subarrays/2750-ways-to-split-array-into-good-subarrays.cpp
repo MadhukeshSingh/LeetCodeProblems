2b14a4d179d0580034534ff1aa0e0ac1bc491424
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-25 09:34:00 +0530

    Time: 329 ms (78.57%), Space: 235.6 MB (35.71%) - LeetHub

---
+++ 2750-ways-to-split-array-into-good-subarrays/2750-ways-to-split-array-into-good-subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+             int mod=1e9+7;
+public:
+
+    int numberOfGoodSubarraySplits(vector<int>& nums) {
+        int n=nums.size();
+        int v[n], p = 0;
+ 
+    for (int i = 0; i < n; i++) {
+        if (nums[i] == 1) {
+            v[p] = i + 1;
+            p++;
+        }
+    }
+ 
+    if (p == 0) return 0;
+ 
+    long long ways = 1;
+    for (int i = 0; i < p - 1; i++) {
+        ways =((ways)%mod * (v[i + 1] - v[i])%mod)%mod;
+    }
+ 
+    return ways; 
+    }
+};
