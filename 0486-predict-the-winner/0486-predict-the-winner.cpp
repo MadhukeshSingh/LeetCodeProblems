194fba0cd6fae8d9074cc248dd1ec97d091dc77a
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-28 22:50:20 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (80.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
-  int solve(vector<int>& nums, int i, int j){
-        if(i > j)
-        return 0;
-        
-        if(i == j)
-        return nums[j];
-
-        int p1 = nums[i] + min(solve(nums, i+2, j), solve(nums, i+1, j-1));
-        int p2 = nums[j] + min(solve(nums, i+1, j-1), solve(nums, i, j-2));
-
-        return max(p1, p2);
+  int dp[21][21]; 
+    bool checkWin(int ans,int total){
+        return ans>=total-ans;
     }
+    int maxScore(vector<int>&A,int total,int i,int j){
+        if(i>j)
+            return 0;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        dp[i][j]=total-min(maxScore(A,total-A[i],i+1,j),maxScore(A,total-A[j],i,j-1));
+        return dp[i][j];
+    }
+public:
     bool PredictTheWinner(vector<int>& nums) {
-        int i = 0;
-        int temp = 0;
-        int j = nums.size()-1;
-
-        for(auto it:nums){
-            temp += it;
-        }
-
-        int p1 = solve(nums, i, j);
-        int p2 = temp-p1;
-
-        return p1 >= p2;
-        
+        int total=0;
+        for(int i=0;i<21;i++)
+            for(int j=0;j<21;j++)
+                dp[i][j]=-1;  
+        for(auto x:nums)
+            total+=x;
+        return checkWin(maxScore(nums,total,0,nums.size()-1),total);
     }
 };
