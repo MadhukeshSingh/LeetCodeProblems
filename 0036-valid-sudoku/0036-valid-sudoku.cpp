2bff50a2167205ce8b84c94eeca798f78ff9919c
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-06-30 23:03:56 +0530

    Time: 36 ms (15.31%), Space: 20.4 MB (18.62%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
+    int coord2gridIdx(int i, int j){
+        return i/3 * 3 + j/3;
+         }
+        
     bool isValidSudoku(vector<vector<char>>& board) {
-          set<pair<int,char>> row,col,grid;
-
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
-                if(board[i][j]!='.'){
-                int count=(i/3)*3 +j/3;
-                
-                if(row.find({i,board[i][j]})==row.end())
-                    row.insert({{i,board[i][j]}});
-                 else return false;
-
-                if(col.find({j,board[i][j]})==col.end())
-                    col.insert({{j,board[i][j]}});
-                 else return false;
-
-                if(grid.find({count,board[i][j]})==grid.end())
-                    grid.insert({{count,board[i][j]}});
-                 else  return false;
-                }
+        vector<set<char>> rows(9), cols(9), grids(9);
+        pair<set<char>::iterator, bool> ret;
+        
+        for(int i = 0; i < 9; ++i){
+            for(int j = 0; j < 9; ++j){
+                char c = board[i][j];
+                if(c == '.') continue;
+                int gridIdx = coord2gridIdx(i, j);
+                ret = rows[i].insert(c);
+                if(!ret.second) return false;
+                ret = cols[j].insert(c);
+                if(!ret.second) return false;
+                ret = grids[gridIdx].insert(c);
+                if(!ret.second) return false;
             }
         }
+        
         return true;
     }
 };
