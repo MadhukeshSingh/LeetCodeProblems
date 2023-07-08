996aa0e787db841bb83935a195bb72d2c4828258
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-07-08 23:05:30 +0530

    Time: 208 ms (68.73%), Space: 69.1 MB (42.61%) - LeetHub

--- 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
+++ 2551-put-marbles-in-bags/2551-put-marbles-in-bags.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-    priority_queue<int> pq1;
-    priority_queue<int, vector<int>, greater<int>> pq2;k--;
-    for (int i = 0; i < weights.size() - 1; i++) {
-        pq1.push(weights[i] + weights[i + 1]);
-        pq2.push(weights[i] + weights[i + 1]);
-        if (pq1.size() > k ) {
-            pq1.pop();
-            pq2.pop();
+     vector<long long> store;
+        for(int i=1;i<weights.size();i++){
+            store.push_back(weights[i]+weights[i-1]);
         }
+        sort(store.begin(),store.end());
+        int n=store.size();
+        long long mini=0;
+        for(int i=0;i<k-1;i++){
+            mini+= store[i];
         }
-       long long ans1 = 0, ans2 = 0;
-       while (!pq1.empty()) {
-        ans1 += pq1.top();
-        ans2 += pq2.top();
-        pq1.pop();
-        pq2.pop();
-       }
-    return ans2 - ans1;
+
+        long long maxi=0;
+        for(int i=0;i<k-1;i++){
+            maxi+= store[n-1-i];
+        }
+        return maxi - mini;
     }
 };
