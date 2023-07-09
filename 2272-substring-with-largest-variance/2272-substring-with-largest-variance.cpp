3b729e4f9480efdce3f6d8c4ccc52ad9935445a4
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-07-10 00:46:50 +0530

    Time: 124 ms (92.45%), Space: 8.2 MB (9.43%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int largestVariance(string s) {
-        vector<int>arr(26);
-        for(auto x:s){
-           arr[x-'a']++; 
-        }
-        int ans=0;
-        for(char i='a';i<='z';i++){
-            for(char j='a';j<='z';j++){
-                 if(j==i||arr[i-'a']==0||arr[j-'a']==0)
-                            continue;
-                for(int k=1;k<=2;k++){
-                   int c1=0;
-                    int c2=0;
-                    for(auto x:s){
-                        if(x==i)
-                            c1++;
-                        if(x==j)
-                            c2++;
-                        if(c2>c1){
-                            c1=0;
-                            c2=0;
-                        }
-                        if(c1>0&&c2>0)
-                        ans=max(ans,c1-c2);
+        int res = 0;
+    unordered_set<char> unique(begin(s), end(s));
+    for (char a : unique)
+        for (char b : unique) {
+            int var = 0, has_b = 0, first_b = 0;
+            for (auto ch : s) {
+                var += ch == a;
+                if (ch == b) {
+                    has_b = true;
+                    if (first_b && var >= 0)
+                        first_b = false;
+                    else if (--var < 0) {
+                        first_b = true;
+                        var = -1;
                     }
-                    reverse(s.begin(),s.end());
                 }
+                res = max(res, has_b ? var : 0);
             }
         }
-        return ans;
+    return res;
     }
 };
