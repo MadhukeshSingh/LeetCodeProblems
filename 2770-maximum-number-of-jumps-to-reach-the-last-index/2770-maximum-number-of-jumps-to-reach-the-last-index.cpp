920b8ef7bb40d735348812555cea2041559cf037
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-07-10 00:36:34 +0530

    Time: 110 ms (91.67%), Space: 61.9 MB (25.00%) - LeetHub

--- 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
+++ 2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.cpp
@@ -1,41 +1,15 @@
 class Solution {
 public:
     int maximumJumps(vector<int>& nums, int target) {
-        int n = nums.size();
-        int less = target * -1;
-        vector<int> dp(n, 0);
-
-        for (int i = n - 2; i >= 0; i--) {
-            int maxJumps = 0;
-            int lastJump = 0;
-
-            for (int j = i + 1; j < n; j++) {
-                int diff = nums[j] - nums[i];
-
-                if (diff >= less && diff <= target) {
-                    if (j < n - 1) {
-                        int jumps = dp[j];
-                        maxJumps = max(maxJumps, jumps);
-                    } else {
-                        if (1 > maxJumps) {
-                            maxJumps = 1;
-                            lastJump = 1;
-                        }
-                    }
-                }
-            }
-
-            if (lastJump == 1) {
-                dp[i] = 1;
-            } else if (maxJumps > 0) {
-                dp[i] = maxJumps + 1;
+       int n = nums.size();
+        vector<int> dp(n,1);
+        dp[0] = 0;
+        for(int i=1;i<n;i++){
+            dp[i] = -1;
+            for(int j=0;j<i;j++){
+                if((abs(nums[i]-nums[j])<=target) && dp[j]!=-1) dp[i] = max(dp[i],1+dp[j]);
             }
         }
-
-        if (dp[0] > 0) {
-            return dp[0];
-        } else {
-            return -1;
-    }
+        return dp[n-1];
     }
 };
