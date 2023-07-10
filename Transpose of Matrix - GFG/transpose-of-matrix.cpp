70e1be1d1eba3a22c821986c2aabcb3960620bf8
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-07-10 18:34:06 +0530

    Added solution - LeetHub

--- Transpose of Matrix - GFG/transpose-of-matrix.cpp
+++ Transpose of Matrix - GFG/transpose-of-matrix.cpp
@@ -10,16 +10,10 @@ class Solution
     void transpose(vector<vector<int> >& matrix, int n)
     { 
         // code here 
-        int x=0;
-        while(x<n)
-        {
-            int f=x;
-            while(f<n)
-            {
-                swap(matrix[x][f],matrix[f][x]);
-                ++f;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = i; j < n; j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
-            ++x;
         }
     }
 };
