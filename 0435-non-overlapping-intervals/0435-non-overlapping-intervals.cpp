b98c66e9c3be27cf0db79a2d9c8cd69ee323b23d
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-07-19 22:19:33 +0530

    Time: 488 ms (61.57%), Space: 89.7 MB (94.24%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,38 +1,22 @@
 class Solution {
 public:
-static bool compare(vector<int> &a, vector<int> &b){
-        if(a[1]==b[1]) return a[0]<b[0];
-        return a[1]<b[1];
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[1] < b[1];
     }
-    
-    int search(vector<vector<int>> &a, int idx){
-        int l=0, r=idx-1;
-        
-        while(l<r){
-            int m=l+(r-l+1)/2;
-            if(a[m][1]<=a[idx][0]){
-                l=m;
-            }else{
-                r=m-1;
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), cmp);
+
+        int prev = 0;
+        int count = 1;
+
+        for(int i = 1; i < n; i++){
+            if(intervals[i][0] >= intervals[prev][1]){
+                prev = i;
+                count++;
             }
         }
-        return (a[l][1]<=a[idx][0] ? l:-1);
+        return n - count;
     }
-    
-    int eraseOverlapIntervals(vector<vector<int>>& a) {
-        sort(a.begin(),a.end(),compare);
-        int n=a.size();
-        
-        vector<int> dp(n,1); 
-
-        int ans=1;
-        for(int i=1;i<n;i++){
-            int idx=search(a,i);
-            dp[i]=max(1,dp[i-1]);
-            if(idx!=-1) dp[i]=max(dp[i],1+dp[idx]);
-            ans=max(ans,dp[i]);
-        } 
-        return n-ans;
-        
-    }
-};
+};  
