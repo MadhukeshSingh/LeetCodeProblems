5986936b569d2a7df2cadbdb8bddb4ae17e146cd
Madhukesh singh <99209636+MadhukeshSingh@users.noreply.github.com>
2023-08-06 10:14:18 +0530

    Time: 2 ms (42.86%), Space: 5.9 MB (57.14%) - LeetHub

--- 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
+++ 2806-account-balance-after-rounded-purchase/2806-account-balance-after-rounded-purchase.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int accountBalanceAfterPurchase(int purchaseAmount) {
-        int x = purchaseAmount % 10;
-        int y = 10 - x, c = 0;
-        if(x < y)
-            c = purchaseAmount - x;
-        else
-            c = purchaseAmount + y;
-        return (100 - c);
+                return 100 - ((purchaseAmount + 5) / 10) * 10;
     }
 };
